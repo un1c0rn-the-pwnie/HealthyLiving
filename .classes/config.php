d8473b6047aaf89000893e9e50b1bfa46ca0c89d
0x476c317463683372 <dimosxakis0@gmail.com>
2021-05-04 15:57:14 +0300

    added setup

--- .classes/config.php
+++ .classes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('db_db', 'localhost');
-define('db_user', 'root');
+define('db_user', 'healthy_living_user');
 define('db_pass', 'QsXPl,10_+;!');
 define('db_charset', 'utf-8');
 define('db_name', 'healthy_living');
---
+++ .classes/setup.php
@@ -0,0 +1,56 @@
+<?php
+
+include 'config.php';
+
+define('root_pass', ''); // Bale ton kwdkio tou root apo phpmyadmin
+define('root_name', 'root'); // bale to onoma tou root apo to phpmyadmin
+
+$link = mysqli_connect(db_db, root_name, root_pass); 
+
+if($link === false){
+    die("Could not connect. " . mysqli_connect_error());
+}
+
+if(mysqli_query($link, "CREATE USER IF NOT EXISTS 'healthy_living_user'@'localhost' IDENTIFIED BY 'QsXPl,10_+;!';")) {
+    echo "User healthy_living_user created successfully<br/>";
+} else {
+    echo "Could not able to execute $sql. " . mysqli_error($link) . "<br/>";
+}
+
+$sql = "CREATE DATABASE healthy_living";
+if(mysqli_query($link, $sql)){
+    echo "Database healthy_living created successfully<br/>";
+} else{
+    echo "Could not able to execute $sql. " . mysqli_error($link) . "<br/>";
+}
+
+if(mysqli_query($link, "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON healthy_living.* TO 'healthy_living_user'@'localhost'")) {
+    echo "Privileges granted to healthy_living_user successfully<br/>";
+} else {
+    echo "Could not able to execute $sql. " . mysqli_error($link) . "<br/>";
+}
+
+mysqli_close($link);
+
+$link = new mysqli(db_db, db_user, db_pass, db_name);
+
+$sql = "CREATE TABLE IF NOT EXISTS `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` varchar(20) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `salt` varchar(32) NOT NULL,
+    `login_hash` varchar(255),
+    `rg_date` DATE NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+  COLLATE latin1_general_cs;";
+
+if(mysqli_query($link, $sql)){
+    echo "Table users created succcessfully<br/>";
+} else{
+    echo "Could not able to execute $sql. " . mysqli_error($link) . "<br/>";
+}
+ 
+mysqli_close($link);
+
+?>
